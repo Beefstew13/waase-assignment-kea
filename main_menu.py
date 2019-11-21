334523952e786f4c8a64f75fb769e6fe1e67f262
Klara Pauline <pauline.klara@gmail.com>
2019-11-21 15:37:33 +0100

    Updated version

--- main_menu.py
+++ main_menu.py
@@ -7,8 +7,18 @@ def main():
 def login():
     username = "BikeShopOwner"
     password = "Hello1234"
-    getusername = input("Please enter username")
-    getpassword = input("Please enter password")
+    getusername = input("""
+--------------------------------------------------
+Please enter username
+--------------------------------------------------
+
+""")
+    getpassword = input("""
+--------------------------------------------------
+Please enter password
+--------------------------------------------------
+
+""")
     if getusername == username and getpassword == password:
         print()
         print('-'*50)
@@ -38,30 +48,3 @@ Please enter your choice: """)
         main_menu()
 
 main()
-
-"""
-
-def main_menu(): # Creates the overall menu, where the user chooses which of the systems, warehouse and sales, the user wishes to go to.
-    print('-'*50) # Prints 50 times - for user experience
-    print('-'*50)
-    print("Press 1 for Sales")
-    print("Press 2 for Warehouse")
-    print("Press any other tab to quit")
-    print('-'*50) # Prints 50 times - for user experience
-    return input() # Prints what the user chooses
-
-main()
-
-while True:
-    choice = main_menu()
-    if choice == "1":
-        print("You chose 1")
-        # import sales python file (the file name should not include the .py)
-
-    elif choice == "2":
-        print("You chose 2")
-        import warehouse
-
-    else:
-        break
-"""
